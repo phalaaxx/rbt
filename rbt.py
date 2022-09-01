e907a988d282ba6d8b3cc290fc945f0e60a6a9ab
Bozhin Zafirov <bozhin@sirius.local>
2022-09-01 22:30:11 +0300

    Add status functionality, initial import.

--- rbt.py
+++ rbt.py
@@ -9,12 +9,23 @@ import collections
 import datetime
 import os
 import pwd
+import re
 import subprocess
 import time
 import typing
 
+import jinja2
 import pytz
 import yaml
+
+import smtplib
+import socket
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+# backups process
 
 # ConfigOptions defines possible configuration options
 ConfigOptions = (
@@ -195,16 +206,236 @@ def load_backups(name: str) -> typing.List[Backup]:
     return backups
 
 
+# backups stats
+
+Completed = collections.namedtuple("Completed", ("duration", "name", "mtime"))
+
+# backup statuses
+StatusText = {
+    0: "OK",
+    1: "ERR",
+    2: "UNK",
+}
+StatusCodes = set()
+
+email_template = jinja2.Template(
+    """
+{% macro ColorStatus(code) %}
+{%- if code == 0 %}<b style="color: green">OK</b>{% endif -%}
+{%- if code == 1 %}<b style="color: red">ERR</b>{% endif -%}
+{%- if code == 2 %}<b style="color: magenta">UNK</b>{% endif -%}
+{% endmacro %}
+{%- for section, data in sections %}
+<h3 style="text-decoration: underline">category: {{section}}</h3>
+<table>
+    <thead>
+        <tr style="background: #ccc">
+            <td width="80px"><b>Status</b></td>
+            <td width="200px"><b>Name</b></td>
+            <td width="200px"><b>Last</b></td>
+            <td width="50px"><b>Duration</b></td>
+        </tr>
+    </thead>
+    <tbody>
+    {%- for code, server, mtime, duration, comment in data %}
+        <tr style="background: #eee">
+            <td style="font-weight: bold">[{{ColorStatus(code)}}]</td>
+            <td>{{server}}</td>
+            <td>{% if mtime %}{{mtime.strftime('%Y/%b/%d %H:%M:%S')}}{% endif %}</td>
+            <td>{{duration}}</td>
+            {% if comment %}<td>{{comment}}</td>{% endif %}
+        </tr>
+    {% endfor -%}
+    </tbody>
+</table>
+{% endfor -%}
+"""
+)
+
+console_template = jinja2.Template(
+    """
+{%- macro ColorStatus(code) %}
+{%- if code == 0 %} [\033[01;32mOK\033[00m] {% endif -%}
+{%- if code == 1 %}[\033[01;31mERR\033[00m] {% endif -%}
+{%- if code == 2 %}[\033[01;35mUNK\033[00m] {% endif -%}
+{% endmacro %}
+{%- for section, data in sections %}
+\033[01;37mcategory: {{section}}\033[00m
+\033[01;33mStatus {{"{:<50}".format("Name")}}{{"{:<21}".format("Last")}}{{"{:<10}".format("Duration")}}\033[00m
+{%- for code, server, mtime, duration, comment in data %}
+ {{ColorStatus(code)-}}
+ {{"{:<50}".format(server)-}}
+ {% if mtime %}{{"{:<21}".format(mtime.strftime('%Y/%b/%d %H:%M:%S'))}}{% else %}{{"{:<21}".format("n/a")}}{% endif -%}
+{{"{:<10}".format(duration)}}{% if comment %}{{comment}}{% endif %}
+{%- endfor %}
+{% endfor -%}
+"""
+)
+
+
+def parse_rfc3339(dt):
+    """Parse RFC3339 datetime string"""
+    broken = re.search(
+        r"([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(\.([0-9]+))?(Z|([+-][0-9]{2}):([0-9]{2}))",
+        dt,
+    )
+    micro = int(broken.group(8))
+    if micro > 999999:
+        micro = micro // 1000
+    return datetime.datetime(
+        year=int(broken.group(1)),
+        month=int(broken.group(2)),
+        day=int(broken.group(3)),
+        hour=int(broken.group(4)),
+        minute=int(broken.group(5)),
+        second=int(broken.group(6)),
+        microsecond=micro,
+        tzinfo=datetime.timezone(
+            datetime.timedelta(
+                hours=int(broken.group(10) or "0"), minutes=int(broken.group(11) or "0")
+            )
+        ),
+    )
+
+
+def read_comment(basedir: str) -> str:
+    """Read comment file"""
+    name = os.path.join(basedir, ".comment")
+    if not os.path.exists(name):
+        return None
+    with open(name, "r") as fh:
+        return fh.read().strip()
+
+
+def read_completed(server: str) -> typing.Optional[typing.List]:
+    """Read completed file"""
+    name = os.path.join(server, "backup.0/completed")
+    if os.path.exists(name) and os.path.isfile(name):
+        with open(name, "r") as fh:
+            data = yaml.load(fh.read(), Loader=yaml.SafeLoader)
+        return Completed(
+            str(datetime.timedelta(seconds=int(data.get("duration")))),
+            data.get("name"),
+            parse_rfc3339(data.get("timestamp")),
+        )
+    return None
+
+
+def get_backup_status(BaseDirList=[]):
+    today = datetime.datetime.now().date()
+    resultSet = []
+    for server in BaseDirList:
+        ServerName = server[8:]
+        # ignore unnecessary files
+        if os.path.isfile(os.path.join(server, ".ignore")):
+            continue
+        # check for status plugin in server backup directory
+        plugin = os.path.join(server, "status")
+        if os.path.isfile(plugin):
+            # status plugins return code 0-3 and output in format:
+            # [OK|ERR|UNK]:[iso|epoch]:<mtime>:{comment}
+            cmd = subprocess.run(["/bin/sh", plugin], capture_output=True)
+            if cmd.returncode in (0, 1, 2):
+                stat, fmt, mtime_str, duration, comment = (
+                    cmd.stdout.decode().strip().split(":")
+                )
+                if fmt.lower() == "iso":
+                    mtime = parse_rfc3339(mtime_str)
+                elif fmt.lower() == "epoch":
+                    mtime = datetime.datetime.fromtimestamp(int(mtime_str))
+                else:
+                    # plugin error, display it
+                    cmd.returncode = 1
+                    mtime = datetime.datetime.now()
+                # save server status
+                resultSet.append((cmd.returncode, ServerName, mtime, duration, comment))
+                StatusCodes.add(StatusText.get(cmd.returncode))
+                continue
+        # retrieve backup status
+        completed = read_completed(server)
+        if completed:
+            if completed.mtime.date() == today:
+                resultSet.append(
+                    (
+                        0,
+                        ServerName,
+                        completed.mtime,
+                        completed.duration,
+                        read_comment(server),
+                    )
+                )
+                StatusCodes.add(StatusText.get(0))
+            else:
+                resultSet.append(
+                    (
+                        1,
+                        ServerName,
+                        completed.mtime,
+                        completed.duration,
+                        read_comment(server),
+                    )
+                )
+                StatusCodes.add(StatusText.get(1))
+            continue
+        resultSet.append((2, ServerName, None, "n/a", read_comment(server)))
+        StatusCodes.add(StatusText.get(2))
+
+    # sort by status
+    resultSet.sort(reverse=True)
+    return resultSet
+
+
+def send_email(Message, Subject, From, To, Server):
+    # prepare root message
+    msgRoot = MIMEMultipart("related")
+    msgRoot["Subject"] = Header(
+        "Servers Backups Status [{0}]".format(",".join(StatusCodes)),
+        "utf-8",
+    ).encode()
+    msgRoot["To"] = To
+
+    # prepare HTML message
+    msgText = MIMEText(
+        Message,
+        _charset="UTF-8",
+        _subtype="html",
+    )
+
+    # attach HTML message
+    msgRoot.attach(msgText)
+
+    # send email
+    mail = smtplib.SMTP(Server)
+    mail.sendmail(From, To, msgRoot.as_string())
+    mail.quit()
+
+
+def get_all_data(root: str = "/backup") -> list:
+    """Prepare and return server backups data"""
+    sections = []
+    for section in os.listdir(root):
+        if not os.path.isdir(f"{root}/{section}"):
+            continue
+        SectionServers = []
+        for server in os.listdir(f"/backup/{section}"):
+            SectionServers.append(f"/backup/{section}/{server}")
+        backup_data = get_backup_status(SectionServers)
+        if len(backup_data):
+            sections.append((section, backup_data))
+    return sorted(sections, key=lambda item: item[0])
+
+
 # main program
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+
     # parse command line arguments
     parser = argparse.ArgumentParser(
         description="Perform rsync based incremental backups"
     )
     subparser = parser.add_subparsers()
-    backup = subparser.add_parser("backup", help="Perform servers backups")
-    stats = subparser.add_parser("stats", help="Get backups statistics")
 
+    backup = subparser.add_parser("backup", help="Perform servers backups")
     backup.add_argument(
         "--tz",
         type=str,
@@ -227,6 +458,38 @@ if __name__ == "__main__":
     backup.add_argument("--server", type=str, help="Backup only specified server")
     backup.add_argument(
         "--verbose", action="store_true", help="Print debug info to stdout"
+    )
+
+    status = subparser.add_parser("status", help="Get backups statistics")
+    status.add_argument(
+        "--smtp",
+        type=str,
+        default="localhost",
+        help="Outgoing SMTP server (default: %(default)s)",
+    )
+    status.add_argument("--mailto", type=str, help="Destination email address")
+    status.add_argument(
+        "--subject",
+        type=str,
+        default="Servers Backups Status",
+        help="Email subject (default: %(default)s)",
+    )
+    status.add_argument(
+        "--from",
+        type=str,
+        default=f"status@{hostname}",
+        help="Email sender address (default: %(default)s)",
+    )
+    status.add_argument(
+        "--console",
+        action="store_true",
+        help="Print statistics on terminal",
+    )
+    status.add_argument(
+        "--root",
+        type=str,
+        default="/backup",
+        help="Backups root path",
     )
 
     # parse arguments
@@ -256,6 +519,17 @@ if __name__ == "__main__":
                         continue
                     verbose_print(f"Starting backup {backup.name}".format(backup.name))
                     backup.run()
-    # path is only defined in stats subcommand
-    elif "path" in cmd_args:
-        pass
+    # smtp is only defined in status subcommand
+    elif "smtp" in cmd_args:
+        if cmd_args.mailto or cmd_args.console:
+            backup_data = get_all_data(cmd_args.root)
+        if cmd_args.mailto:
+            send_email(
+                email_template.render(sections=backup_data),
+                "Servers Backups Status",
+                "status@backup1.cloxter.net",
+                cmd_args.mailto,
+                cmd_args.smtp,
+            )
+        if cmd_args.console:
+            print(console_template.render(sections=backup_data))
